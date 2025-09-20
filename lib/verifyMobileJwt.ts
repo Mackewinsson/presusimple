1b74cdb9ddd7245b33c4f1fefd5acad4d7270d21
Mackewinsson Palencia <mackewinsson@gmail.com>
2025-09-20 17:19:59 +0200

    remove auth logic on mobile

--- lib/verifyMobileJwt.ts
+++
@@ -1,9 +0,0 @@
-// lib/verifyMobileJwt.ts
-import { jwtVerify } from "jose";
-
-const secret = new TextEncoder().encode(process.env.MOBILE_JWT_SECRET!);
-
-export async function verifyMobileJwt(token: string) {
-  const { payload } = await jwtVerify(token, secret);
-  return payload as { sub: string; email?: string };
-}
